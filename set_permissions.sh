f5c297c850c370e55717fe004c7eb9493a0c9d36
Srilekha S <86847655+SrilekhaS20@users.noreply.github.com>
2024-12-14 13:07:44 +0530

    Changes to enable executable permissions to shell scripts

---
+++ set_permissions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Make build.sh executable
+chmod +x build.sh
+
+# Make run_tests.sh executable
+chmod +x run.sh
+
+echo "Permissions for build.sh and run.sh have been set to executable."
